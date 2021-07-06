440f656ac6deab0f05912feb940c12cd353c8dd0
jmorton138 <jmorton138@gmail.com>
2021-07-06 14:58:49 +0100

    End game if three in a row method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,9 +1,11 @@
 class Player
     attr_reader :name
     attr_reader :symbol
+    
     def initialize(name, player_num)
         @name = name
         @player_num = player_num
+        @player_score
         if player_num == 1
             @symbol = "X"
         else
@@ -14,10 +16,17 @@ class Player
 end
 
 class GameBoard
-    @@victory = false
+    # attr_accessor :player1_score
+    # attr_accessor :player2_score
     attr_accessor :board
+
+    @@victory = false
+    @@winning_combos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+
     def initialize
         @board = [1, 2, 3, 4, 5, 6]
+        @player1_score = []
+        @player2_score = []
         
     end
 
@@ -26,11 +35,18 @@ class GameBoard
         board.each {|num| puts num.to_s + "|"}
     end
 
-    def player_move
+    def player_move(player)
         until @@victory == true do
             puts "Player 1, you're move"
             player_1_move = gets.chomp.to_i
+            player1_score.push(player_1_move)
             update_game(player_1_move, "X")
+            game_status
+            
+            # puts "Player 2, you're move"
+            # player_2_move = gets.chomp.to_i
+            # update_game(player_2_move, "O")
+            # game_status("0")
         end
     end
     #after player makes move, render new board
@@ -45,6 +61,16 @@ class GameBoard
         end
         render_board
     end
+
+    def game_status
+        @@winning_combos.each do |combo|
+            if combo.all?  {|num| player1_score.include?(num)}
+                @@victory = true
+            end
+        end 
+
+    end
+
 end
 
 
